e7d50c5f678b27a5c5934b0c815a78672a524247
Alexander <stdAlexTikhonov@hotmail.com>
2021-10-29 00:07:24 +0300

    fix

--- include/processor.h
+++ include/processor.h
@@ -6,8 +6,8 @@ class Processor {
   float Utilization();  // TODO: See src/processor.cpp
                         // TODO: Declare any necessary private members
  private:
-  long prev_active_ = 1, prev_total_ = 1;
-  long active_ = 2, total_ = 2;
+  long prev_active_ = 0, prev_total_ = 0;
+  long active_ = 0, total_ = 0;
 };
 
 #endif
