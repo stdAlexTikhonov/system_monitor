54dc9b0b04f935a7d1a53b7c32e6718b5b8f1238
Alexander <stdAlexTikhonov@hotmail.com>
2021-11-01 19:18:18 +0300

    reading user for processes

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -224,7 +224,23 @@ string LinuxParser::Uid(int pid) {
 
 // TODO: Read and return the user associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::User(int pid [[maybe_unused]]) { return string(); }
+string LinuxParser::User(int pid) {
+  string uid = Uid(pid);
+  string user_name, x, read_uid, line;
+  std::ifstream filestream(kPasswordPath);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::replace(line.begin(), line.end(), ':', ' ');
+      std::istringstream linestream(line);
+      while (linestream >> user_name >> x >> read_uid) {
+        if (read_uid == uid) {
+          return user_name;
+        }
+      }
+    }
+  }
+  return string();
+}
 
 // TODO: Read and return the uptime of a process
 // REMOVE: [[maybe_unused]] once you define the function
--- src/process.cpp
+++ src/process.cpp
@@ -28,7 +28,7 @@ string Process::Command() { return string(); }
 string Process::Ram() { return string(); }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return LinuxParser::Uid(pid_); }
+string Process::User() { return LinuxParser::User(pid_); }
 
 // TODO: Return the age of this process (in seconds)
 long int Process::UpTime() { return 0; }
